b86356050eb274a39b3bfc5e7a0d7d1db9206505
rahulgundala007 <95472879+rahulgundala007@users.noreply.github.com>
2023-05-02 16:07:05 -0400

    Added new route feature

--- src/components/BusReservation.jsx
+++ src/components/BusReservation.jsx
@@ -15,9 +15,14 @@ function BusReservation() {
   const navigate = useNavigate();
  // const [name, setName] = useState("");
  // const [tickets, setTickets] = useState(0);
+  const [route, setRoute] = useState("")
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [date, setDate] = useState(new Date());
+
+  const handleRouteChange = (event) => {
+    setRoute(event.target.value);
+  }
 
   const handleFromChange = (event) => {
     setFrom(event.target.value);
@@ -33,13 +38,13 @@ function BusReservation() {
 
 
   const handleBook = async () => {
-    if ( from ===""|| to === "" || date === "") {
+    if ( route === "" || from ===""|| to === "" || date === "") {
       toast.current.show({severity:'error', summary: 'Error', detail:'Please enter all details', life: 2000});
       return;
     }else{
       try {
         console.log('selected date ',date);
-        const response = await searchBus(from,to,date);
+        const response = await searchBus(route,from,to,date);
         if(response.message === 'Found'){
             const busId = response.bus.bus_id;
             navigate(`/booking/${busId}`);  
@@ -63,9 +68,9 @@ function BusReservation() {
       </div>
       <h1>Bus Search</h1>
       <div>
-        <label>From:</label>
-        <select value={from} onChange={handleFromChange}>
-        <option value="">Select origin</option>
+        <label>Route:</label>
+        <select value={route} onChange={handleRouteChange}>
+        <option value="">Select Route</option>
         <option value="Arbutus">Arbutus</option>
         <option value="Arundel">Arundel</option>
         <option value="BWI MARC">BWI MARC</option>
@@ -75,10 +80,78 @@ function BusReservation() {
         <option value="Route 40">Route 40</option>
         </select>
       </div>
+
+      <div>
+      <label>From:</label>
+  {route === "Paradise" ? (
+    <select value={from} onChange={handleFromChange}>
+      <option value="">Select origin</option>
+      <option value="Paradise1">Paradise1</option>
+      <option value="Paradise2">Paradise2</option>
+      <option value="Paradise3">Paradise3</option>
+      <option value="Paradise4">Paradise4</option>
+    </select>
+  ) : route === "Route 40" ? (
+    <select value={from} onChange={handleFromChange}>
+      <option value="">Select origin</option>
+      <option value="Route 40-1">Route 40-1</option>
+      <option value="Route 40-2">Route 40-2</option>
+      <option value="Route 40-3">Route 40-3</option>
+      <option value="Route 40-4">Route 40-4</option>
+    </select>
+  ) : route === "Downtown" ? (
+    <select value={from} onChange={handleFromChange}>
+      <option value="">Select origin</option>
+      <option value="Downtown1">Downtown1</option>
+      <option value="Downtown2">Downtown2</option>
+      <option value="Downtown3">Downtown3</option>
+      <option value="Downtown4">Downtown4</option>
+    </select>
+    ) : route === "Catonsville" ? (
+      <select value={from} onChange={handleFromChange}>
+        <option value="">Select origin</option>
+        <option value="Catonsville1">Catonsville1</option>
+        <option value="Catonsville2">Catonsville2</option>
+        <option value="Catonsville3">Catonsville3</option>
+        <option value="Catonsville4">Catonsville4</option>
+      </select>
+      ) : route === "BWI MARC" ? (
+        <select value={from} onChange={handleFromChange}>
+          <option value="">Select origin</option>
+          <option value="BWI MARC1">BWI MARC1</option>
+          <option value="BWI MARC2">BWI MARC2</option>
+          <option value="BWI MARC3">BWI MARC3</option>
+          <option value="BWI MARC4">BWI MARC4</option>
+        </select>
+      ) : route === "Arundel" ? (
+        <select value={from} onChange={handleFromChange}>
+          <option value="">Select origin</option>
+          <option value="Arundel1">Arundel1</option>
+          <option value="Arundel2">Arundel2</option>
+          <option value="Arundel3">Arundel3</option>
+          <option value="Arundel4">Arundel4</option>
+        </select>
+        ) : route === "Arbutus" ? (
+          <select value={from} onChange={handleFromChange}>
+            <option value="">Select origin</option>
+            <option value="Arbutus1">Arbutus1</option>
+            <option value="Arbutus2">Arbutus2</option>
+            <option value="Arbutus1">Arbutus3</option>
+            <option value="Arbutus2">Arbutus4</option>
+          </select>
+  ) :(
+    <select value={from} onChange={handleFromChange}>
+      <option value="">Select origin</option>
+      </select>
+  )}
+  
+</div>
+      
       <div>
         <label>To:</label>
         <select value={to} onChange={handleToChange}>
         <option value="">Select destination</option>
+        
         <option value="Arbutus">Arbutus</option>
         <option value="Arundel">Arundel</option>
         <option value="BWI MARC">BWI MARC</option>
